84f784f4d4184c1c3f86f6e449035d7a7de8d75b
nodasgithub <47042341+nodasgithub@users.noreply.github.com>
2020-10-18 16:58:39 -0400

    Initial commit

---
+++ app/src/main/java/com/example/actionbarexample/MainActivity2.java
@@ -0,0 +1,24 @@
+package com.example.actionbarexample;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setIcon(R.mipmap.chrome);
+        actionBar.setTitle(" Second Activity");
+        actionBar.setDisplayUseLogoEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+
+    }
+}
